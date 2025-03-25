ec03d3a66e4c2c8efd4836e5bedf734f71e216a0
yashc2002 <yash.ch@navtech.io>
2025-03-25 16:33:15 +0530

    updates

--- campaign_automation.py
+++ campaign_automation.py
@@ -176,7 +176,7 @@ def fetch_page_content(url):
     """
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        "Referer": "https://discplusprofiles.com/",
+        "Referer": url,
         "Accept-Language": "en-US,en;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
         "Connection": "keep-alive"
